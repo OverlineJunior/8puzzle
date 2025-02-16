94f2be4af03eede6e0eefb4c11de6f8f60135a4d
over <franciscosebastianyjunior@hotmail.com>
2025-02-16 17:20:37 -0300

    Implement CLI arguments to pick which algorithm to use

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from heuristics import manhattan_distance
 from shared import _
-from search_algorithms import search_with_astar
+from search_algorithms import search_with_dfs, search_with_bfs, search_with_gbf, search_with_astar, SearchResult
 from puzzle_io import read_puzzle, write_search_results
+from argparse import ArgumentParser
 
 initial = read_puzzle()
 
@@ -12,6 +13,22 @@ goal = (
 	(13, 14, 15, _),
 )
 
-match search_with_astar(initial, goal, manhattan_distance):
-	case path, visited_count:
-		write_search_results(path, visited_count)
+parser = ArgumentParser()
+parser.add_argument("algorithm", choices=["DFS", "BFS", "GBF", "A*"])
+
+if __name__ == "__main__":
+	args = parser.parse_args()
+
+	search_result: SearchResult = None
+	match args.algorithm:
+		case "DFS":
+			search_result = search_with_dfs(initial, goal)
+		case "BFS":
+			search_result = search_with_bfs(initial, goal)
+		case "GBF":
+			search_result = search_with_gbf(initial, goal, manhattan_distance)
+		case "A*":
+			search_result = search_with_astar(initial, goal, manhattan_distance)
+
+	if search_result:
+		write_search_results(*search_result)
