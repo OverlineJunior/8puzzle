54aa457ab4f9649edcf61458c88116a2543a9b82
over <franciscosebastianyjunior@hotmail.com>
2025-02-16 16:30:10 -0300

    - Move input and result txt files to their own io directory - Add io/results.txt to .gitignore

--- puzzle_io.py
+++ puzzle_io.py
@@ -2,6 +2,9 @@ import math
 from graph import Path, pretty_path
 from shared import Puzzle, _, pretty_puzzle
 
+INPUT_PATH = "io/input.txt"
+RESULTS_PATH = "io/results.txt"
+
 def square_matrix[T](arr: list[T]) -> list[list[T]]:
 	"""
 	Converts a 1D list into a 2D square matrix.
@@ -25,23 +28,23 @@ def read_puzzle() -> Puzzle:
 		elif tile == "X":
 			return 0
 		else:
-			raise ValueError(f"Invalid tile `{tile}` in `input.txt`")
+			raise ValueError(f"Invalid tile `{tile}` in `{INPUT_PATH}`")
 
-	with open("input.txt") as file:
+	with open(INPUT_PATH) as file:
 		tiles = [parse(tile) for tile in file.read().split()]
 		l = len(tiles)
 
 		if l == 9 or l == 16 or l == 25:
 			return tuple(tuple(row) for row in square_matrix(tiles))
 		else:
-			raise ValueError(f"Expected `input.txt` to have 9, 16, or 25 tiles, got {len(tiles)}")
+			raise ValueError(f"Expected `{INPUT_PATH}` to have 9, 16, or 25 tiles, got {len(tiles)}")
 
 def write_search_results(path: Path[Puzzle], visited_count: int):
 	"""
 	Writes the search results to a file in the root directory.
 	"""
 
-	with open("results.txt", "w") as file:
+	with open(RESULTS_PATH, "w") as file:
 		file.write(f"Nodes visited: {visited_count}\n")
 		file.write("\nPath:\n")
 		file.write(pretty_path(path, pretty_puzzle))
